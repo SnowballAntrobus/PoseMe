c9d6133dc77681263c4d4d9ba39ef804409b79e2
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2022-02-17 19:28:54 -0600

    Initial Commit

---
+++ PoseMe/PoseMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoseMeApp.swift
+//  PoseMe
+//
+//  Created by Dante Gil-Marin on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PoseMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
